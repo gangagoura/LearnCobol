35314f6727023d07c89f587666788791877aaa82
GUILHERME FERNANDES NETO <40431403+gangagoura@users.noreply.github.com>
2021-07-28 18:29:17 -0300

    Create HelloCobolWorld.cbl
    hello cobol

---
+++ HelloCobolWorld.cbl
@@ -0,0 +1,11 @@
+* Prints predefined message	
+	IDENTIFICATION DIVISION.
+	PROGRAM-ID. PreDefMsg.
+	AUTHOR. gangagoura.
+	DATA DIVISION.	
+	WORKING-STORAGE SECTION.
+	01 PreDefMsg	PIC X(18) VALUE 'Hello Cobol World!'.
+	
+	PROCEDURE DIVISION.
+	DISPLAY PreDefMsg.
+	STOP RUN.
